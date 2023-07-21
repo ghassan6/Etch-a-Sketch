728dd3dcc4a97ef4104f137d180779f92d8bf092
ghassan6 <ghassansalameh166@gmail.com>
2023-07-21 15:32:00 +0300

    make the CSS and HTML file , add two functions
    add "erase" and "black" function and the basic grid

---
+++ script.js
@@ -0,0 +1,66 @@
+
+
+const container = document.querySelector(".container");
+
+function createGrid (row , col) {
+
+
+    for (let i = 0 ; i < row ; i++)
+    {
+        let rows = document.createElement('div')
+        rows.classList.add('row')
+        container.appendChild(rows)
+        rows.style.display = 'flex'
+
+        for (let j = 0 ; j < col ; j++)
+        {
+            let cell = document.createElement('div')
+            cell.textContent = j + 1;
+            cell.classList.add('cell')
+            rows.appendChild(cell)
+        }
+    }
+}
+
+createGrid(10 , 10);
+
+let cells = document.querySelectorAll('.cell');
+let eraser = document.querySelector('.eraser')
+let colors = document.querySelector('.color')
+
+
+
+colors.addEventListener('click' , () => {
+    cells.forEach((cell) => {
+
+        cell.addEventListener('mouseover' , () =>
+        {
+            cell.style.backgroundColor = 'black'
+        });
+    });
+})
+
+// function color() {
+//     cells.forEach((cell) => {
+
+//         cell.addEventListener('mouseover' , () =>
+//         {
+//             cell.style.backgroundColor = 'black'
+//         });
+//     });
+// }
+
+
+eraser.addEventListener('click' , () => {
+    cells.forEach((cell) => {
+
+        cell.addEventListener('mouseover' , () =>
+        {
+            cell.style.backgroundColor = 'white'
+        });
+    });
+})
+
+
+
+
